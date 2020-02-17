203ed9d6f09e5a4f3c7d8ec1f4e27cb97eb5fefe
Alexey Meleshkevich <meleshhhkevich@icloud.com>
2020-02-17 11:13:28 +0300

    Fixed json data model

---
+++ NBAlufe/Models/DecodableStructs/DataFilters.swift
@@ -0,0 +1,9 @@
+//
+//  DataFilters.swift
+//  NBAlufe
+//
+//  Created by Alexey Meleshkevich on 17.02.2020.
+//  Copyright © 2020 Alexey Meleshkevich. All rights reserved.
+//
+
+import Foundation
