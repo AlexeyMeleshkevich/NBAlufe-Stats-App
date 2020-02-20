06bb52ec1af5ddc680d5696aadde4147e3d1f973
Alexey Meleshkevich <meleshhhkevich@icloud.com>
2020-02-20 21:35:13 +0300

    Created struct for flexible using url

--- NBAlufe/Models/Networking/NetworkingStructs/UrlData.swift
+++ NBAlufe/Models/Networking/NetworkingStructs/UrlData.swift
@@ -10,8 +10,10 @@ import Foundation
 
 struct URLData {
     var urlKey = "2020-02-14"
-    
     var url = "https://api-nba-v1.p.rapidapi.com/games/date/"
+    
+    static let httpHeader = ["x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+                             "x-rapidapi-key": "d3f4042bc4msh3e8baa6aaf0e778p15af52jsn260491400f82"]
     
     init(urlKey: String) {
         self.urlKey = urlKey
