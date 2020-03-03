bed3f177c86ec16e23ceb2d483521afbd5451f8b
Alexey Meleshkevich <meleshhhkevich@icloud.com>
2020-03-03 21:05:29 +0300

    Changed launchscreen

--- NBAlufe/Application/Controllers/LaunchScreenController/LaunchScreenController.swift
+++ NBAlufe/Application/Controllers/LaunchScreenController/LaunchScreenController.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+import Gifu
+
+class LaunchScreenViewController: UIViewController {
+    
+    let mainVC = MainNavigationController()
+    
+    private lazy var gifView: GIFImageView = {
+        let view = GIFImageView()
+        
+        view.contentMode = .scaleAspectFit
+        view.animate(withGIFNamed: "tenor.gif")
+        view.startAnimatingGIF()
+
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+        self.view.addSubview(gifView)
+        
+        setGifViewConstraints(sender: gifView)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.navigationController?.pushViewController(self.mainVC, animated: true)
+        }
+    }
+    
+    private func setGifViewConstraints(sender: GIFImageView) {
+        sender.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            sender.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            sender.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0),
+            sender.widthAnchor.constraint(equalToConstant: 260),
+            sender.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+}
